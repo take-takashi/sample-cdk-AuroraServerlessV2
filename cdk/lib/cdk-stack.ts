56b0c9e6c7de5facff46fc08c13e226bc9d03c3b
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-16 15:35:07 +0000

    vpcの作成とBastionHostEc2の作成

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { aws_ec2 as ec2 } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create vpc
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      natGateways: 0,
+      maxAzs: 2,
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create private subnets
+    const privateSubnets = vpc.selectSubnets({
+      subnetGroupName: "private-subnet",
+      subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+    });
+
+    // create security group for BastionHost
+    const ec2Sg = new ec2.SecurityGroup(this, "Ec2Sg", {
+      vpc: vpc,
+      securityGroupName: "BastionHostSg",
+    });
+
+    // create ec2 for BationHost
+    const bastionHostEc2 = new ec2.BastionHostLinux(this, "BastionHostEc2", {
+      vpc: vpc,
+      instanceName: "BastionHostEc2ForRDS",
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      subnetSelection: privateSubnets,
+      securityGroup: ec2Sg,
+    });
   }
 }
