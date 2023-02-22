461967feb5202035d15d1ec010ef6992f41b89cb
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-22 15:41:25 +0000

    テスト的にDockerfile利用

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,10 +6,14 @@ WORKSPACE=$PWD
 cd ${WORKSPACE}/cdk
 npm ci
 
+# npm install for app
+cd ${WORKSPACE}/app
+npm ci
+
 # install start-session-plugin
-cd /tmp
-curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
-sudo dpkg -i session-manager-plugin.deb
+#cd /tmp
+#curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+#sudo dpkg -i session-manager-plugin.deb
 
 # create .env file
 cd ${WORKSPACE}
