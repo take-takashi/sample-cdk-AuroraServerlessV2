49019da0263d19fae5c4b8230c79262f14a49884
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-25 13:28:12 +0000

    テーブル定義作成

---
+++ app/prisma/migrations/20230225131243_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
