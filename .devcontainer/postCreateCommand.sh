b86392506835025a0370b17c0c3133917891ebe4
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-25 14:20:59 +0000

    env修正

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,16 +10,14 @@ npm ci
 cd ${WORKSPACE}/app
 npm ci
 
-# install start-session-plugin
-#cd /tmp
-#curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
-#sudo dpkg -i session-manager-plugin.deb
-
 # create .env file
 cd ${WORKSPACE}/app
 echo '' > .env
-echo AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} >> .env
-echo AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} >> .env
-echo AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} >> .env
-# for prisma
-echo DATABASE_URL=${PRD_DATABASE_URL} >> .env
+## .env for prisma
+echo DATABASE_URL=${DEV_DATABASE_URL} >> .env
+
+# create .env.prd file
+cd ${WORKSPACE}/app
+echo '' > .env.prd
+## .env for prisma
+echo DATABASE_URL=${PRD_DATABASE_URL} >> .env.prd
