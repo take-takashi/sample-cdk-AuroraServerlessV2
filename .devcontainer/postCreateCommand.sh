ed7c5c9c295a593385e5441828f010c7c8e195de
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-25 13:28:01 +0000

    prisma用に修正

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -16,8 +16,10 @@ npm ci
 #sudo dpkg -i session-manager-plugin.deb
 
 # create .env file
-cd ${WORKSPACE}
+cd ${WORKSPACE}/app
 echo '' > .env
 echo AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} >> .env
 echo AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} >> .env
 echo AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} >> .env
+# for prisma
+echo DATABASE_URL=${PRD_DATABASE_URL} >> .env
